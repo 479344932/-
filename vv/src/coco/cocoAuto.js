73de0002d9a9c689bd466755e8459b119d6ab3ff
zilv <547740900@qq.com>
2017-02-05 15:09:26 +0800

    2

---
+++ vv/src/coco/cocoAuto.js
@@ -0,0 +1,13 @@
+var coco=coco||{};
+coco.F2xAuto_0=function(){
+	var s = this;
+	F2xContainer.call(s);
+	s.initUI();
+};
+F2xExtend(coco.F2xAuto_0,F2xContainer);
+coco.F2xAuto_0.prototype.initUI=function(){
+	var s = this;
+	var _d0=Flash2x.s({type:0,data:"A/ncvMAAAg5dMA/PAAAMAAAA5dg"},{type:0,color:"#000000"},null);
+	Flash2x.d(_d0,{x:202.5,y:184});
+	s.addChild(_d0);
+};
